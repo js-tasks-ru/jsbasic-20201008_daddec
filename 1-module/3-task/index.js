fe0990b290e411f7c5bb27a784904b22eef01772
Sergiy Makal <daddec@gmail.com>
2020-10-27 21:45:50 +0300

    создал две функции первая работает с регистром,а вторая с пустой строкой

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,6 +3,25 @@
  * @param {string} str
  * @returns {string}
  */
+
+
 function ucFirst(str) {
-  // ваш код...
+  
+  if(isValid(str)){
+    let a = str[0].toUpperCase();
+    let b = str.slice( 1 );
+    c = a + b;
+    return c;
+  }
+return false;
+}
+
+function isValid(str) {
+  if (str == ('')  ){
+    return false;
+  } 
+
+return true;
+  
+  
 }
