e8994144a13301aa63e7e1ebb828ef12a2d3fa2a
Sergiy Makal <daddec@gmail.com>
2020-11-02 22:38:33 +0300

    перебрал объект сравнил на наличие числа и числа сумировал

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,21 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
-  // ваш код...
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+let b = 0;
+for(let key in salaries){
+  let a = (salaries[key]);
+  if(typeof a == 'number'){
+     b += a;
+    
+  } 
+  console.log(b)
 }
